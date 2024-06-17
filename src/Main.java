934d87ca94e5120d8c69bcae528bb84679f880cb
krollchristensen <krollchristensen@gmail.com>
2024-06-17 08:43:53 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Deklaration og initialisering af variabler
+        int num1 = 15;
+        int num2 = 4;
+
+        // Udfør aritmetiske operationer
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        // Udskriv resultaterne
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
